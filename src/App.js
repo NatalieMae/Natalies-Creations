ec4cca642168d59f83dbf2e693074d90147b0d3a
Natalie <nataliemae263@gmail.com>
2022-07-25 19:10:38 -0500

    Start

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [data, setData] = React.useState(null);
+
+  React.useEffect(() => {
+    fetch("/api")
+    .then((res) => res.json())
+    .then((data) => setData(data.message));
+  },
+  []);
+  
   return (
     <div className="App">
       <header className="App-header">
