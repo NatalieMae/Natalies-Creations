93ede2509ac0946e595fe3062a254d9b577637d4
Natalie <nataliemae263@gmail.com>
2022-08-12 01:38:32 -0500

    more stuff

--- src/pages/Flowers/Flowers.js
+++ src/pages/Flowers/Flowers.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link, useMatch, useResolvedPath } from 'react-router-dom';
 // import PhotoUploader from "./PhotoUpLoader";
 
 
@@ -7,7 +8,15 @@ function Flowers() {
     return (
         <>
         <div className="flowers">
+        {/* <ul>
+                <CustomLink to="/finalproject/src/pages/Home.js">Home</CustomLink>
+            </ul> */}
         <div className="flower-one">
+            <h1>
+                <CustomLink to="/finalproject/src/pages/Flowers/Photos/Hostaflowers.jpg">
+                    Purple Pop Flowers
+                </CustomLink>
+            </h1>
             </div>
         <div className="flower-two">
             </div>
@@ -26,6 +35,20 @@ function Flowers() {
         </div>
         </>
     )
+        }
+
+
+    function CustomLink({to, children,  ...props }) {
+        const resolvedPath = useResolvedPath(to)
+        const isActive = useMatch({ path: resolvedPath.pathname, end: true})
+    
+        return (
+            <li className={isActive ? "active" : "" }>
+                <Link to={to} {...props}>
+                    {children}
+                </Link>
+            </li>
+        )
     
 }
 
